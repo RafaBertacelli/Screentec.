5e7b35d0eae2c5750860998998112d422f97201c
Theuz <112184110+MatheusSantosSilv@users.noreply.github.com>
2024-03-20 23:32:37 -0300

    Add files via upload

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,30 +4,93 @@ import styles from './styles.js';
 import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
 
-
-
 export default function Home() {
 
   const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
-      <View style={styles.LoginCaixa}>
-        <View style={styles.LoginTitlulo}>
-          <Text style={styles.titulo}>
-            HOME
-          </Text>
+      <View style={styles.nav}>
+        <View>
+          <TouchableOpacity>
+            <Image
+              source={require('./../../../images/Menu.png')}
+            />
+          </TouchableOpacity>
         </View>
-        <View style={styles.LoginBotao}>
-          <TouchableOpacity style={styles.botaoLogin}>
-            <Text style={styles.textbotaoLogin}>FAÇA LOGIN</Text>
+        <View style={styles.icon}>
+          <Image
+            style={styles.icon}
+            source={require('./../../../images/screentec.png')}
+          />
+        </View>
+        <View style={styles.icon}>
+          <TouchableOpacity>
+            <Image
+              source={require('./../../../images/User.png')}
+            />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botaoLoginCadastreSe}>
-            <Text style={styles.textbotaoLoginCadastreSe}>CADASTRE-SE</Text>
+        </View>
+        <View style={styles.icon}>
+          <TouchableOpacity>
+            <Image
+              source={require('./../../../images/Search.png')}
+            />
           </TouchableOpacity>
+        </View>
+      </View>
+      <View>
+        <TouchableOpacity style={styles.setaDir}>
+          <Image
+            style={styles.setaDir}
+            source={require('./../../../images/setaDir.png')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.setaEsq}>
+          <Image
+            style={styles.setaEsq}
+            source={require('./../../../images/setaEsq.png')}         
+          />
+        </TouchableOpacity>
+        <Image
+          style={styles.carrosel}
+          source={require('./../../../images/wakanda.png')}          
+        />
+        <Image
+          style={styles.carroselHolder}
+          source={require('./../../../images/carrosel_placeholder.png')}
+        />
+      </View>
+      <View style={styles.card}>
+        <View>
+          <Text style={styles.textoCard}>Em alta</Text>
+        </View>
+        <View style={styles.cards}>
+          <View>
+            <Image
+              style={styles.imgFilme}
+              source={require('./../../../images/kungFuPanda4.jpg')}
+            />
+            <Text style={styles.tituloCards}>
+              Kung Fu Panda 4 
+            </Text>
+          </View>
+        </View>
+        <View style={styles.cards}>
+          <View>
+            <Image
+              style={styles.imgFilme}
+              source={require('./../../../images/bobMarley.jpg')}
+            />
+            <Text style={styles.tituloCards}>
+              Bob Marley
+            </Text>
+            <Text>
+              SINOPSE
+            </Text>
+          </View>
         </View>
       </View>
     </View>
   );
-
 }
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -3,19 +3,101 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
     container:{
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#292929',
         alignItems: 'center',
-        justifyContent: 'center',
+        position: 'relative'    
     },      
     LoginPipoca:{
         flex:1,
         display:"flex",
         alignItems:"center",
         justifyContent:"center",
-        paddingTop:'100px',
     },
     imagemPipoca:{
         width:100,
         height:100,
     },
+    nav: {
+        backgroundColor: '#930707',
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: '100%',
+        height: '7%',   
+        justifyContent: 'space-between'
+    },
+    textoCard:{
+        flex:1,
+        color:'#ffffff',
+        fontSize: 20,
+        paddingTop: 1,
+        zIndex: 1,
+        position: 'absolute',
+        marginTop: 10
+    },
+    imgCard:{
+        width: 300,
+        height: 170,
+        marginTop: 50,
+        alignSelf: 'flex-end' 
+    },
+    carrosel:{
+        marginTop: '10%',
+        width: 330,
+        borderRadius: 10,
+        
+    },
+    carroselHolder:{
+        width: 330,
+        height: 15,
+        marginTop: 4
+    },
+    setaEsq: {
+        position:  'absolute',
+        width: 25,
+        height: 25,
+        marginTop: 60,
+        zIndex: 1
+    },
+    setaDir: {
+        position:  'absolute',
+        width: 25,
+        height: 25,
+        marginTop: 60,
+        zIndex: 1,
+        alignSelf: 'flex-end'
+    },
+    icon:{
+        
+    },
+    cards: {
+        marginTop: 40,
+        backgroundColor: '#161617',
+        width: 300,
+        height: 170,
+        borderRadius: 10,
+    },
+    imgFilme:{
+      width: 100,
+      height: 170,
+      borderRadius: 10,
+      borderBottomRightRadius: 0,
+      borderTopRightRadius: 0
+    },
+    card:{
+        
+    },
+    tituloCards: {
+        marginTop: 4,
+        color:'#fff',
+        fontSize: 12,
+        position: 'absolute',
+        alignSelf: 'center',
+        fontFamily: 'League Spartan'
+    },
+    sinopse: {
+        textAlign: 'right',
+        position: 'absolute',
+        alignSelf: 'center',
+        color: '#fff'
+    }
 })
